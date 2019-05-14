800b2efa1fbf1d9fdc633fa75b5075255f406cb2
J4R1 <36295134+J4R1@users.noreply.github.com>
2019-05-14 14:20:49 +0300

    /utils/

---
+++ utils/database.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mysql = require('mysql2');
+
+const connect = () => {
+  // create the connection to database
+  const connection = mysql.createPool({
+    connectionLimit : 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+  });
+  return connection;
+  console.log("Connected!");
+};
+
+module.exports = {
+  connect: connect,
+};
